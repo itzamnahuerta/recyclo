9b40a94cb8bb0961f4c96f7a85f61987af2b9666
Jack Gay <jackpowis@gmail.com>
2019-05-11 15:47:02 -0400

    second commit: completed the three basic tables, working on the table relationships

--- server/db/models.js
+++ server/db/models.js
@@ -18,6 +18,35 @@ const Location = db.define('location', {
     longitude: Sequelize.INTEGER
 })
 
+const Material = db.define('material', {
+    name: Sequelize.STRING
+})
+
+const User = db.define('user', {
+    name: Sequelize.STRING,
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  })
+
+  User.beforeCreate(async (user, options) => {
+    const hashedPassword = await bcrypt.hash(user.password, 12)
+  user.password = hashedPassword
+});
+
+User.hasMany(Material)
+
 module.exports = {
-    Location
+    Location, 
+    Material,
+    User
 }
