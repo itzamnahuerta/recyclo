d29c4274754aae6569ce46e319923d63a7581c26
Jack Gay <jackpowis@gmail.com>
2019-05-12 17:12:19 -0400

    trialled the test and made some changes to models

---
+++ server/db/seed.js
@@ -0,0 +1,53 @@
+const { Material, Location } = require('./models');
+
+const main = async() => {
+    await Material.destroy({
+        where: {}
+    });
+    await Location.destroy({
+        where: {}
+    });
+
+    const metal = await Material.create({
+        name: 'Metal'
+    });
+
+    const glass = await Material.create({
+        name: 'Glass'
+    });
+
+    const paper = await Material.create({
+        name: 'Paper'
+    });
+
+    const plastic = await Material.create({
+        name: 'Plastic'
+    });
+
+    const newStyle = await Location.create({
+        name: 'New Style Recycling',
+        phone_number: '7183264175',
+        url: 'http://newstylerecycling.com/index.html',
+        postal_code: '11378',
+        latitude: '40.741895',
+        longitude: '-73.989308'
+    })
+
+    await newStyle.addMaterial(glass)
+    await newStyle.addMaterial(metal)
+    await newStyle.addMaterial(paper)
+
+} 
+
+async function run() {
+    try {
+      await main();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await process.exit()
+    }
+  }
+  
+  run();
+
