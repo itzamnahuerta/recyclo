88979b90cdfcb006d43f90a4a9b188b2a8ebc3ee
Itzamna <huerta.ek@gmail.com>
2019-05-13 14:40:18 -0400

    created components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import './App.css';
+@import "./styles/App.scss";
+import Header from './components/Header/Header'
+import Materials from './components/Materials/Materials'
+// import Map from './components/Map/Map'
+// import UserProfile from './components/UserProfile/UserProfile'
+
+
 
 function App() {
   return (
     <div className="App">
+    <h1> recyclo. </h1>
+    <Header />
+    <Materials />
 
     </div>
   );
---
+++ client/src/components/Materials/Materials.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './Materials.css'
+
+export default class Materials extends Component {
+  render() {
+    return (
+      <div className='Materials'>
+        <div className="Plastic"> Plastic </div>
+
+        <div className="Metals"> Metals </div>
+        <div className="Textile"> Textile </div>
+        <div className="MixPaper">Mix-Paper </div> 
+      </div>
+    )
+  }
+}
