dc7661e835df05f64bc45eba695d586a86c1ebd0
Jack Gay <jackpowis@gmail.com>
2019-05-14 10:27:57 -0400

    refactoring pin info js code

---
+++ client/src/components/Map/pin-info.js
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react';
+
+class CrimeInfo extends PureComponent {
+
+  render() {
+
+
+    return (
+      <div>          
+      </div>
+    )
+
+  }
+}
+
+export default PinInfo
